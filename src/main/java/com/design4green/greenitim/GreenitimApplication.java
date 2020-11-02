947624cc5f989ef3530859fe8e90a856f87ba37e
andrei zaharia <andrejzaha@yahoo.com>
2020-11-02 21:26:48 +0200

    added Spring boot generated project

---
+++ src/main/java/com/design4green/greenitim/GreenitimApplication.java
@@ -0,0 +1,13 @@
+package com.design4green.greenitim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenitimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenitimApplication.class, args);
+	}
+
+}
