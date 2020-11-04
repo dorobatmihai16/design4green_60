47931b77219efa502003154f8e384da48529790a
andrei zaharia <andrejzaha@yahoo.com>
2020-11-05 00:41:23 +0200

    wip: quintiles calculation

--- src/main/java/com/design4green/digital/service/impl/QuintilesServiceImpl.java
+++ src/main/java/com/design4green/digital/service/impl/QuintilesServiceImpl.java
@@ -5,6 +5,7 @@ import com.design4green.digital.model.Quintile;
 import com.design4green.digital.repository.QuintileRepository;
 import com.design4green.digital.service.CityStatisticsService;
 import com.design4green.digital.service.QuintilesService;
+import com.design4green.digital.utils.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -58,7 +59,64 @@ public class QuintilesServiceImpl implements QuintilesService {
     }
 
     private Optional<Quintile> getQuintileFromCityStatistics(CityStatistics cityStatistics) {
-        return Optional.empty();
+        if (cityStatisticsService.isInvalidCityStatistics(cityStatistics)) {
+            return Optional.empty();
+        }
+
+        Quintile quintile = new Quintile();
+
+        double accesAuxInterfacesRaw =
+                ( ( (1 - cityStatistics.getTauxDeCouvertureHdThd()) / Constants.CONST_TAUX_COUVERTURE_HD ) * 100 )
+                +  ( ( (1 - cityStatistics.getTauxDeCouvertureMobile()) / Constants.CONST_TAUX_COUVERTURE_MOBILE ) * 100 )
+                +  ( ( ( (cityStatistics.getTauxDePauvrete() - Constants.CONST_TAUX_PAUVRETE) / Constants.CONST_TAUX_PAUVRETE ) + 1 ) * 100 )
+                +  ( ( ( (Constants.CONST_REVENUS_MEDIAN - cityStatistics.getRevenusMedianDisponible()) / Constants.CONST_REVENUS_MEDIAN ) + 1 ) * 100 );
+        double accesAuxInterfacesValue = (accesAuxInterfacesRaw * 100) / ( 4 * 100);
+        quintile.setAccessAuxInterfaces(getAccesAuxInterfacesKPI(accesAuxInterfacesValue));
+
+        double servicesPublicIndiv = 2
+                - ( ( cityStatistics.getServicesPublicIndividu() - Constants.CONST_PART_SERVICES_PUBLIC_INDIV ) / Constants.CONST_PART_SERVICES_PUBLIC_INDIV )
+                + 1;
+        if (servicesPublicIndiv < 0) {
+            servicesPublicIndiv = 0;
+        }
+        double accesInformationRaw =
+                ( ( ( (cityStatistics.getPartDesFamillesMonoparentales() - Constants.CONST_PART_FAMILLES_MONO) / Constants.CONST_PART_FAMILLES_MONO ) + 1 ) * 100 )
+                + ( ( ( (cityStatistics.getPartDesMenagesUnePersone() - Constants.CONST_PART_MENAGES_PERSONNE) / Constants.CONST_PART_MENAGES_PERSONNE ) + 1 ) * 100 )
+                + servicesPublicIndiv;
+        double accesInformationValue = ( accesInformationRaw * 100 ) / ( 3 * 100);
+        quintile.setAccessAInformation(accesInformationValue);
+
+        double accessGlobalRaw = accesAuxInterfacesRaw + accesInformationRaw;
+        double accessGlobalValue = (accessGlobalRaw * 100) / ( 7 * 100);
+        quintile.setAccessGlobal(getAccesAInformationKPI(accessGlobalValue));
+
+        return Optional.of(quintile);
+    }
+
+    private double getAccesAuxInterfacesKPI(double accesAuxInterfacesValue) {
+        if (accesAuxInterfacesValue < 21.22) {
+            return 1d;
+        } else if (accesAuxInterfacesValue >= 21.22 && accesAuxInterfacesValue < 26.27) {
+            return 2d;
+        } else if (accesAuxInterfacesValue >= 26.27 && accesAuxInterfacesValue < 31.31) {
+            return 3d;
+        } else if (accesAuxInterfacesValue >= 31.31 && accesAuxInterfacesValue < 36.36) {
+            return 4d;
+        }
+        return 5d;
+    }
+
+    private double getAccesAInformationKPI(double accesAuxInterfacesValue) {
+        if (accesAuxInterfacesValue < 56.54) {
+            return 1d;
+        } else if (accesAuxInterfacesValue >= 56.54 && accesAuxInterfacesValue < 78.95) {
+            return 2d;
+        } else if (accesAuxInterfacesValue >= 78.95 && accesAuxInterfacesValue < 101.36) {
+            return 3d;
+        } else if (accesAuxInterfacesValue >= 101.36 && accesAuxInterfacesValue < 123.77) {
+            return 4d;
+        }
+        return 5d;
     }
 
 }
---
+++ src/main/java/com/design4green/digital/utils/Constants.java
@@ -0,0 +1,13 @@
+package com.design4green.digital.utils;
+
+public class Constants {
+
+    public static final Double CONST_TAUX_COUVERTURE_HD = 0.963;
+    public static final Double CONST_TAUX_COUVERTURE_MOBILE = 0.970;
+    public static final Double CONST_TAUX_PAUVRETE = 0.172;
+    public static final Integer CONST_REVENUS_MEDIAN = 19672;
+    public static final Double CONST_PART_FAMILLES_MONO = 0.159;
+    public static final Double CONST_PART_MENAGES_PERSONNE = 0.37;
+    public static final Double CONST_PART_SERVICES_PUBLIC_INDIV = 0.000351261251592481;
+
+}
